053870ecb8ca136b130f5196e0f19e2eed2ca8c3
moamel-hashim <muamal1997@gmail.com>
2023-09-12 06:24:00 -0700

    added font awesome

--- nav-bar/src/components/navbar.jsx
+++ nav-bar/src/components/navbar.jsx
@@ -1,17 +1,19 @@
 import { Link } from "react-router-dom";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 function Nav() {
   return (
     <nav className="background">
-      <div className="flex w-100">
+      <div className="flex w-100 justify-content-evenly">
         <div className="w-75">
           <h1>Navbar</h1>
         </div>
         <div>
           <ul className="flex gap">
-            <li><Link to='/home'>Home</Link></li>
-            <li><Link to='/link'>Link</Link></li>
-            <li>Dropdown</li>
+            <li><Link to='/home' className="link">Home</Link></li>
+            <li><Link to='/link' className="link">Link</Link></li>
+            <li>Dropdown<FontAwesomeIcon icon={faChevronRight}/></li>
             <ul className="hidden">
               <li>action</li>
               <li>another action</li>
