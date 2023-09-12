92b2c87bfb6c456eb0fe6e1bf4e22f8cffc0d45c
moamel-hashim <muamal1997@gmail.com>
2023-09-12 16:52:14 -0700

    adding some functionality to the page

--- nav-bar/src/components/navbar.jsx
+++ nav-bar/src/components/navbar.jsx
@@ -1,25 +1,42 @@
 import { Link } from "react-router-dom";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faChevronRight} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
 
 function Nav() {
+  const [isShown,setIsShown] = useState(false);
+  console.log(setIsShown)
   return (
     <nav className="background">
       <div className="flex w-100 justify-content-evenly">
-        <div className="w-75">
+        <div className="w-50">
           <h1>Navbar</h1>
         </div>
-        <div>
-          <ul className="flex gap">
-            <li><Link to='/home' className="link">Home</Link></li>
-            <li><Link to='/link' className="link">Link</Link></li>
-            <li>Dropdown<FontAwesomeIcon icon={faChevronRight}/></li>
-            <ul className="hidden">
-              <li>action</li>
-              <li>another action</li>
-              <li>more actions</li>
-            </ul>
-            <li>Disabled</li>
+        <div className="w-50">
+          <ul className="flex justify-content-center">
+            <div className="padding-1rem">
+              <li><Link to='/home' className="link">Home</Link></li>
+            </div>
+            <div className="padding-1rem">
+              <li><Link to='/link' className="link">Link</Link></li>
+            </div>
+            <div className="padding-1rem flex gap" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+              <li>Dropdown
+              <div className={isShown === true ? 'nested-list' : 'hidden'}>
+                <ul>
+                  <li>action</li>
+                  <li>another action</li>
+                  <li>more actions</li>
+                </ul>
+              </div>
+              </li>
+              <div>
+                <FontAwesomeIcon icon={faChevronRight} className={isShown === true ? 'font-awesome' : ''}/>
+              </div>
+            </div>
+            <div className="padding-1rem">
+              <li>Disabled</li>
+            </div>
           </ul>
         </div>
       </div>
